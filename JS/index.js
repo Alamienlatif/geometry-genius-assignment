37ceadc011b03453d1695f457f45d02b8f5dbc82
Alamien <alamienlatif3@gmail.com>
2023-06-26 20:57:58 +0600

    all code calculation

--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,21 @@
-// document.getElementById("triangle-btn")
-// .addEventListener("click", function (){
-//     const quantityBase = document.getElementById("btrianglequantity").value;
-//     const quantity = document.getElementById("htrianglequantity").value;
-//     const triangleTotal = quantityBase * quantity * 0.5;
-//     // console.log(triangleTotal);
-//   setInnerText("area",triangleTotal);
-// });
+document.getElementById("triangle-btn")
+.addEventListener('click', function (){
+    const quantityBase = document.getElementById("btrianglequantity").value;
+    const quantity = document.getElementById("htrianglequantity").value;
+    const triangleTotal = quantityBase * quantity * 0.5;
+    // console.log(triangleTotal 'it's ok');
+  // setInnerText("area",triangleTotal);
+});
 
 
-// document.getElementById("rectangle-btn")
-// .addEventListener("click", function (){
-//     const quantityBase = document.getElementById("wrectanglequantity").value;
-//     const quantity = document.getElementById("lrectanglequantity").value; 
-//     const rectangleTotal = quantityBase * quantity;
-//     // console.log(rectangleTotal);
-//     setInnerText("area",rectangleTotal);
-// });
+document.getElementById("rectangle-btn")
+.addEventListener("click", function (){
+    const quantityBase = document.getElementById("wrectanglequantity").value;
+    const quantity = document.getElementById("lrectanglequantity").value; 
+    const rectangleTotal = quantityBase * quantity;
+    // console.log(rectangleTotal it's ok);
+    // setInnerText("area",rectangleTotal);
+});
 
 
 //   function setInnerText(id, value) {
@@ -28,36 +28,36 @@ document.getElementById("parallelogram-btn")
   const quantityBase = document.getElementById("parallelogramquantityb").innerText;
   const quantity = document.getElementById("parallelogramquantityh").innerText;
   const parallelogramTotal = quantityBase * quantity;
-  // console.log(parallelogramTotal);
-  document.getElementById("area").innerText = parallelogramTotal;
+  console.log(parallelogramTotal);
+  // document.getElementById("area").innerText = parallelogramTotal;
 });
 
 
-// document.getElementById("rhombus-btn")
-// .addEventListener("click", function (){
-//   const quantityBase = document.getElementById("rhombus1").innerText;
-//   const quantity = document.getElementById("rhombus2").innerText;
-//  const rhombusTotal = quantityBase * quantity * 0.5;
-// console.log(rhombusTotal);  
-//   // document.getElementById("area").innerText = rhombusTotal;
-// });
+document.getElementById("rhombus-btn")
+.addEventListener("click", function (){
+  const quantityBase = document.getElementById("rhombus1").innerText;
+  const quantity = document.getElementById("rhombus2").innerText;
+ const rhombusTotal = quantityBase * quantity * 0.5;
+console.log(rhombusTotal);  
+  // document.getElementById("area").innerText = rhombusTotal;
+});
 
 
-// document.getElementById("pentagon-btn")
-// .addEventListener("click", function (){
-//   const quantityBase = document.getElementById("pentagonp").innerText;
-//   const quantity = document.getElementById("pentagonb").innerText;
-//   const pentagonTotal = quantityBase * quantity;
-//   console.log(pentagonTotal);
-// })
+document.getElementById("pentagon-btn")
+.addEventListener("click", function (){
+  const quantityBase = document.getElementById("pentagonp").innerText;
+  const quantity = document.getElementById("pentagonb").innerText;
+  const pentagonTotal = quantityBase * quantity;
+  console.log(pentagonTotal);
+})
 
-// document.getElementById("ellipse-btn")
-// .addEventListener("click", function (){
-//   const quantityBase = document.getElementById("ellipse1").innerText;
-//   const quantity = document.getElementById("ellipse2").innerText;
-//   const ellipseTotal = quantityBase * quantity;
-//   console.log(ellipseTotal);
-// })
+document.getElementById("ellipse-btn")
+.addEventListener("click", function (){
+  const quantityBase = document.getElementById("ellipse1").innerText;
+  const quantity = document.getElementById("ellipse2").innerText;
+  const ellipseTotal = quantityBase * quantity;
+  console.log(ellipseTotal);
+})
 
 // function displayData(productName, productprice, productQuanteity, priceTotal)  {
 //   const container = document.getElementById("table-container");
